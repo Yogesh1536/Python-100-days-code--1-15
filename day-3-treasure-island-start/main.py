301c9bdd9f3b35520d5a147cb0fffd3f74033bdb
Yogesh1536 <114746494+Yogesh1536@users.noreply.github.com>
2022-10-04 22:41:47 +0530

    Add files via upload

---
+++ day-3-treasure-island-start/main.py
@@ -0,0 +1,44 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+print('In first stage of the game you want to chosse Left or Right, to move further')
+level_1 = input('Type any of L or R\n')
+if level_1 == 'L':
+  print('There is a river, you want to cross it, in this case you want to wait for boat otherwise will swim to cross the river')
+  level_2 = input('Enter the letter S to swim and W to wait for boat\n')
+  if level_2 == 'W':
+    print('You can see three colour door as red,yellow,green, in this which door you will pass')
+    level_3 = input('please enter first letter of the colour in caps\n')
+    if level_3 == 'Y':
+      print('You win the Game')
+    else:
+      print('Game Over')
+  else:
+    print('Game Over')
+else:
+  print('Game Over')
